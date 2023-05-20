b1c0cce71413389c789f71390e892bf730198c75
Viktoriia <pecourses>
2023-05-20 19:43:03 +0300

    Add transactions slice

--- client/src/pages/TransactionsPage/TransansactionsPage.jsx
+++ client/src/pages/TransactionsPage/TransansactionsPage.jsx
@@ -2,10 +2,36 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 
 function TransansactionsPage () {
+  const transactions = [
+    { amount: 10, operationType: 'INCOME', date: '2023-01-01' },
+    { amount: 10, operationType: 'INCOME', date: '2023-01-01' },
+  ];
+
   return (
     <div>
       <Header />
-      <main>TransansactionsPage</main>
+      <main>
+        TransansactionsPage
+        <table>
+          <caption>Finance Operations</caption>
+          <thead>
+            <tr>
+              <th key={1}>Amount</th>
+              <th key={2}>Operation Type</th>
+              <th key={3}>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map(t => (
+              <tr key={t.id}>
+                <td>{t.amount}</td>
+                <td>{t.operationType}</td>
+                <td>{t.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
